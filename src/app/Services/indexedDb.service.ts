da45269d285cd20174f4eb9d5245932f93e3547a
stefanoyoyo <git.stefano@outlook.it>
2022-03-30 12:54:56 +0200

    implementato salvataggio su indexedDb

--- src/app/Services/indexedDb.service.ts
+++ src/app/Services/indexedDb.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from "@angular/core";
 export class IndexedDbService {
 
   public save(toSave: number) {
-       
+       this.createObjectStore('ciao', 'mondo' ) 
   }
 
   // #region API
@@ -21,6 +21,31 @@ export class IndexedDbService {
 
   /**Creating table into the specified database */
   public createObjectStore(nameIndexedDb: string, nameObjectStore: string ) {
+
+    // Opening the dtaabase and getting the promise 
+    var request = window.indexedDB.open(nameIndexedDb, 1);
+
+    // This handler is called when a new version of the database
+    // is created, either when one has not been created before
+    // or when a new version number is submitted by calling
+    // window.indexedDB.open().
+    // This handler is only supported in recent browsers.
+    request.onupgradeneeded = event => {
+      var db = Object.assign(event.target).result;
+
+      // Create an objectStore for this database
+      var objectStore = db.createObjectStore(nameObjectStore, { keyPath: "taskTitle" });
+
+      // define what data items the objectStore will contain
+      objectStore.createIndex("hours", "hours", { unique: false });
+      objectStore.createIndex("minutes", "minutes", { unique: false });
+      objectStore.createIndex("day", "day", { unique: false });
+      objectStore.createIndex("month", "month", { unique: false });
+      objectStore.createIndex("year", "year", { unique: false });
+
+      objectStore.createIndex("notified", "notified", { unique: false });
+    };
+
 
   }
 
